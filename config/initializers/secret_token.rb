8a7ea06028e06758ae9c315ab486eb93b9db7cc7
frederick.mao(毛凌志) <frederick.mao@gmail.com>
2011-10-08 23:01:41 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CrawlerLinks::Application.config.secret_token = 'c8d4e66ec745b97d208f7dc2eb0dac909fe18d70e7a535eaa1b4b74e08f34fc3a2225a81e148ef0be192aba206d5653cf33132c2abeeb363cb44e483493f0fb7'
